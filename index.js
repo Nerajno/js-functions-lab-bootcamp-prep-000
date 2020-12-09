f34a21f975ef56fc5bc4468c120853421b39feb5
nerajno <nerajno@gmail.com>
2020-12-09 07:48:45 -0500

    commit tree 1f1bc24edce220c4418e0844fb108e55d3ef9d9e

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // write your code below!
+// function  happyHolidays(string) {
+//     return "Happy holidays!";
+// }
+
+
+function happyHolidays(){
+    let greeting = "Happy holidays!";
+    return greeting
+  }
+
+function happyHolidaysto(name) {
+    let greeting = "Happy holidays";
+    return `greeting, ${name}!`  
+}
